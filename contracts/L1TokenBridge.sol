2ccc9ebcd78c71c9497c3d07c2f0a4ad77051acc
Tomas Oliver <tomi204@outlook.com>
2024-12-15 02:55:16 -0300

    fix: fix contracts

--- contracts/L1TokenBridge.sol
+++ contracts/L1TokenBridge.sol
@@ -3,30 +3,58 @@ pragma solidity ^0.8.21;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@eth-optimism/contracts/L1/messaging/CrossDomainEnabled.sol";
+import "@eth-optimism/contracts/L1/messaging/L1CrossDomainMessenger.sol";
 
-contract L1TokenBridge is Ownable, CrossDomainEnabled {
+contract L1TokenBridge is Ownable {
     mapping(address => bool) public supportedTokens;
+    L1CrossDomainMessenger public l1Messenger;
+    address public l2TokenBridge;
 
     event DepositInitiated(
         address indexed token,
         address indexed from,
         uint256 amount
     );
+
     event WithdrawalFinalized(
         address indexed token,
         address indexed to,
         uint256 amount
     );
 
-    constructor(
-        address _l1CrossDomainMessenger
-    ) CrossDomainEnabled(_l1CrossDomainMessenger) Ownable(msg.sender) {}
+    /**
+     * @param _l1CrossDomainMessenger Dirección del mensajero en L1
+     *        (ejemplo: 0x5086d1eEF304eb5284A0f6720f79403b4e9bE294 en Mainnet)
+     * @param _l2TokenBridge Dirección del contrato puente en L2
+     */
+    constructor(address _l1CrossDomainMessenger, address _l2TokenBridge) {
+        require(_l1CrossDomainMessenger != address(0), "Invalid L1 messenger");
+        require(_l2TokenBridge != address(0), "Invalid L2 bridge");
 
+        l1Messenger = IL1CrossDomainMessenger(_l1CrossDomainMessenger);
+        l2TokenBridge = _l2TokenBridge;
+    }
+
+    /**
+     * @dev Permite actualizar la dirección del puente en L2.
+     */
+    function setL2TokenBridge(address _l2TokenBridge) external onlyOwner {
+        require(_l2TokenBridge != address(0), "Invalid L2 bridge");
+        l2TokenBridge = _l2TokenBridge;
+    }
+
+    /**
+     * @dev Añade un token soportado para el puente entre L1 y L2.
+     */
     function addSupportedToken(address token) external onlyOwner {
         supportedTokens[token] = true;
     }
 
+    /**
+     * @dev Inicia un depósito a L2 bloqueando tokens en L1 y enviando un mensaje al puente en L2.
+     * @param token Dirección del token en L1
+     * @param amount Cantidad a depositar
+     */
     function depositToL2(address token, uint256 amount) external {
         require(supportedTokens[token], "Token not supported");
 
@@ -38,21 +66,35 @@ contract L1TokenBridge is Ownable, CrossDomainEnabled {
             amount
         );
 
-        sendCrossDomainMessage(
-            l2TokenBridge, // Dirección del contrato bridge en L2
+        // Envía el mensaje al contrato puente en L2 a través del mensajero L1.
+        l1Messenger.sendMessage(
+            l2TokenBridge,
             message,
-            1000000 // gas limit
+            1_000_000 // Ajustar el gas limit según sea necesario
         );
 
         emit DepositInitiated(token, msg.sender, amount);
     }
 
+    /**
+     * @dev Finaliza un retiro desde L2, liberando tokens en L1 al destinatario.
+     * Esta función debe ser llamada por el mensajero L1, actuando en nombre del contrato en L2.
+     * @param to Dirección del usuario que recibe los tokens en L1
+     * @param token Dirección del token en L1
+     * @param amount Cantidad a retirar
+     */
     function finalizeWithdrawal(
         address to,
         address token,
         uint256 amount
-    ) external onlyFromCrossDomainAccount(l2TokenBridge) {
+    ) external {
         require(supportedTokens[token], "Token not supported");
+
+        // Verifica que el remitente del mensaje sea el contrato puente en L2 a través del mensajero L1.
+        require(
+            l1Messenger.xDomainMessageSender() == l2TokenBridge,
+            "Not authorized sender"
+        );
 
         IERC20(token).transfer(to, amount);
 
--- contracts/L2TokenBridge.sol
+++ contracts/L2TokenBridge.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.8.21;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@eth-optimism/contracts/L2/messaging/CrossDomainEnabled.sol";
+import "@eth-optimism/contracts/L2/messaging/L2CrossDomainMessenger.sol";
 
-contract L2TokenBridge is Ownable, CrossDomainEnabled {
+contract L2TokenBridge is Ownable {
     mapping(address => address) public l1TokenToL2Token;
-    mapping(address => uint256) public pendingWithdrawals;
+    address public l1TokenBridge; // Address of the bridge contract in L1
+    L2CrossDomainMessenger public l2Messenger;
 
     event TokensBridged(
         address indexed l1Token,
@@ -15,16 +16,36 @@ contract L2TokenBridge is Ownable, CrossDomainEnabled {
         address indexed user,
         uint256 amount
     );
+
     event WithdrawalInitiated(
         address indexed l1Token,
         address indexed user,
         uint256 amount
     );
 
-    constructor(
-        address _l2CrossDomainMessenger
-    ) CrossDomainEnabled(_l2CrossDomainMessenger) Ownable(msg.sender) {}
+    /**
+     * @param _l2CrossDomainMessenger 0x4200000000000000000000000000000000000007 Mainnet Optimism)
+     * @param _l1TokenBridge L1TokenBridge address
+     */
+    constructor(address _l2CrossDomainMessenger, address _l1TokenBridge) {
+        require(_l2CrossDomainMessenger != address(0), "Invalid L2 messenger");
+        require(_l1TokenBridge != address(0), "Invalid L1 token bridge");
 
+        l2Messenger = IL2CrossDomainMessenger(_l2CrossDomainMessenger);
+        l1TokenBridge = _l1TokenBridge;
+    }
+
+    /**
+     * @dev Allows updating the L1 bridge contract address.
+     */
+    function setL1TokenBridge(address _l1TokenBridge) external onlyOwner {
+        require(_l1TokenBridge != address(0), "Invalid L1 token bridge");
+        l1TokenBridge = _l1TokenBridge;
+    }
+
+    /**
+     * @dev Registers the relationship between an L1 token and its L2 counterpart.
+     */
     function registerToken(
         address l1Token,
         address l2Token
@@ -36,21 +57,37 @@ contract L2TokenBridge is Ownable, CrossDomainEnabled {
         l1TokenToL2Token[l1Token] = l2Token;
     }
 
+    /**
+     * @dev Transfers tokens in L2 to the user. Assumes this contract already owns these tokens in L2.
+     * @param l1Token Address of the token in L1
+     * @param amount Amount of tokens to deliver in L2
+     */
     function bridgeToL2(address l1Token, uint256 amount) external {
         address l2Token = l1TokenToL2Token[l1Token];
         require(l2Token != address(0), "Token not supported");
+        require(amount > 0, "Amount must be > 0");
 
+        // Transfers L2 tokens to the user. Assumes the contract already has them.
         IERC20(l2Token).transfer(msg.sender, amount);
 
         emit TokensBridged(l1Token, l2Token, msg.sender, amount);
     }
 
+    /**
+     * @dev Initiates a withdrawal to L1, locking the tokens in L2 and sending a message to L1
+     *      to finalize the withdrawal there.
+     * @param l1Token Address of the token in L1
+     * @param amount Amount to withdraw
+     */
     function initiateWithdrawal(address l1Token, uint256 amount) external {
         address l2Token = l1TokenToL2Token[l1Token];
         require(l2Token != address(0), "Token not supported");
+        require(amount > 0, "Amount must be > 0");
 
+        // The user must have previously approved this amount of l2Token to this contract.
         IERC20(l2Token).transferFrom(msg.sender, address(this), amount);
 
+        // Creates the message that will be sent to L1 to finalize the withdrawal.
         bytes memory message = abi.encodeWithSignature(
             "finalizeWithdrawal(address,address,uint256)",
             msg.sender,
@@ -58,11 +95,9 @@ contract L2TokenBridge is Ownable, CrossDomainEnabled {
             amount
         );
 
-        sendCrossDomainMessage(
-            l1TokenBridge, // Dirección del contrato bridge en L1
-            message,
-            1000000 // gas limit
-        );
+        // Sends the message through the messenger to L1.
+        // Adjust the gasLimit (e.g., 1_000_000) as needed.
+        l2Messenger.sendMessage(l1TokenBridge, message, 1_000_000);
 
         emit WithdrawalInitiated(l1Token, msg.sender, amount);
     }
